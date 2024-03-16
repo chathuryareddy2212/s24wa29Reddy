057053ff51c4d61e322747a40b602f67d53d51d4
chathuryareddy2212 <156229804+chathuryareddy2212@users.noreply.github.com>
2024-03-16 16:34:21 -0500

    made changes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+let product = 1;
+let factor = 1;
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  factor++;
+  product *= factor;
+  res.send(`Product is: ${product}`);
 });
 
 module.exports = router;
